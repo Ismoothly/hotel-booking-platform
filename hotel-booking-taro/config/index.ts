5ec9f8e45c7e3104290853509a61ee1a4b531fea
Ismoothly <1844252306@qq.com>
2026-02-23 22:02:09 +0800

    移植更新后小程序UI页面

--- hotel-booking-taro/config/index.ts
+++ hotel-booking-taro/config/index.ts
@@ -1,5 +1,6 @@
 import { defineConfig, type UserConfigExport } from '@tarojs/cli'
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import { UnifiedWebpackPluginV5 } from 'weapp-tailwindcss/webpack'
 import devConfig from './dev'
 import prodConfig from './prod'
 
@@ -53,6 +54,17 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       },
       webpackChain(chain) {
         chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
+        chain.merge({
+          plugin: {
+            install: {
+              plugin: UnifiedWebpackPluginV5,
+              args: [{
+                appType: 'taro',
+                rem2rpx: true
+              }]
+            }
+          }
+        })
       }
     },
     h5: {
